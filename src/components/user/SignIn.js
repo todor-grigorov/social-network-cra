6e16d1cc2ac3aa54e9738ea02e6a1c955076f40f
Todor Grigorov <t.grigorov99@gmail.com>
2021-03-19 23:05:49 +0200

    Added Material UI package
    Added Material UI package.
    Used Material UI in Sign In page .

--- src/components/user/SignIn.js
+++ src/components/user/SignIn.js
@@ -1,33 +1,49 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { Grid, Paper, TextField, makeStyles, Typography } from '@material-ui/core';
 import '../../css/App.css';
 
 
-const SignIn = (props) => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+    paper: {
+        width: "100%",
+        height: "100%",
+    }
+}));
 
+const SignIn = (props) => {
+    const classes = useStyles();
 
     return (
-        <div className="signin-page">
-            <div className="signin-container">
-                <form id="signin-form" method="POST">
-                    <h1>Sign in</h1>
-                    <p>Your programming social network</p>
-                    <div>
-                        <label htmlFor="usernmae">Username</label>
-                        <input id="username" type="text" name="username" placeholder="USERNAME" />
-                    </div>
-                    <div>
-                        <label htmlFor="usernmae">Password</label>
-                        <input id="password" type="password" name="password" placeholder="PASSWORD" />
-                    </div>
+        <Grid container className="signin-page">
+            <Grid item xs={2} className="signin-container">
+                <Paper container className={classes.paper}>
+                    <form id="signin-form" method="POST">
+                        <Typography variant="h1" component="h1" >Sign in</Typography >
+                        <Typography variant="h6" component="h6">Your programming social network</Typography>
+                        <Grid container spacing={2} justify="center">
+                            <Grid item xs={10}>
+                                <TextField variant="outlined" fullWidth label="Username" name="username" />
+                            </Grid>
+                            <Grid item xs={10}>
+                                <TextField variant="outlined" fullWidth type="password" label="Password" name="password" />
+                            </Grid>
+                        </Grid>
 
-                    <button type="submit">Sign in</button>
-                </form>
-            </div>
+                        <button type="submit">Sign in</button>
+                    </form>
+                </Paper>
+            </Grid>
             <div>
                 <span>New member? <Link to="/register" className="join-now-btn">Join now</Link></span>
             </div>
-        </div>
+        </Grid>
     );
 }
 
