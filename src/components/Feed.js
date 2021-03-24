cb4268f4a2865289d48b086c55a77258d5dd83a6
Todor Grigorov <t.grigorov99@gmail.com>
2021-03-24 23:30:55 +0200

    Posts and Firebase
    React App is integrated with Firebase's Firestore.
    User can now add Post.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../css/Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
 import ImageIcon from '@material-ui/icons/Image';
@@ -6,16 +7,47 @@ import EventNoteIcon from '@material-ui/icons/EventNote';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import InputOption from './InputOption';
+import Post from './Post';
+import { db } from '../firebase/firebase';
+import firebase from 'firebase';
 
 function Feed() {
+
+    const [posts, setPosts,] = useState([]);
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        db.collection("post").orderBy("timestamp", "desc").onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => (
+                {
+                    id: doc.id,
+                    data: doc.data(),
+                }
+            )))
+        });
+    }, []);
+
+    const sendPost = (e) => {
+        e.preventDefault();
+        db.collection("post").add({
+            name: 't.grigorov',
+            description: 'just a test',
+            message: input,
+            photoUrl: '',
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+
+        setInput("");
+    };
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon />
                     <form action="">
-                        <input type="text" placeholder="Make a post" />
-                        <button type="submit">Send</button>
+                        <input type="text" placeholder="Make a post" value={input} onChange={(e) => { setInput(e.target.value) }} />
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -25,6 +57,17 @@ function Feed() {
                     <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7fc15e" />
                 </div>
             </div>
+            {posts.map(({ id, data: { name, description, message, photoUrl } }) => {
+                return (
+                    <Post
+                        key={id}
+                        name={name}
+                        description={description}
+                        message={message}
+                    />
+                )
+            }
+            )}
         </div>
     )
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC428VyUHH0jhuKftLrNN-mw9u_tPJAODw",
+    authDomain: "social-network-cra.firebaseapp.com",
+    projectId: "social-network-cra",
+    storageBucket: "social-network-cra.appspot.com",
+    messagingSenderId: "44275772299",
+    appId: "1:44275772299:web:604a0341f18e3fa29f1d8e"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
