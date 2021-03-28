43a7ee8bf53afd2eff0d7cc938b5ec05aed74147
Todor Grigorov <t.grigorov99@gmail.com>
2021-03-28 23:00:31 +0300

    Added Programming Quote Widget
    Added Quote Widget that makes an API cal to free API for Programming Quotes.

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -5,10 +5,13 @@ import '../css/Widgets.css';
 
 const Widgets = () => {
     const [jobs, setJobs] = useState([]);
+    const [quote, setQuote] = useState();
     const [loadingJobs, setLoadingJobs] = useState(true);
+    const [loadingQuote, setLoadingQuote] = useState(true);
 
     useEffect(() => {
         fetchJobs();
+        fetchQuetes();
     }, []);
 
     const fetchJobs = () => {
@@ -24,28 +27,65 @@ const Widgets = () => {
             .catch((err) => alert(err.message));
     };
 
+    const fetchQuetes = () => {
+        if (loadingQuote)
+            setLoadingQuote(true);
+
+        fetch(config.urls.randomProgrammingQuote)
+            .then((res) => res.json())
+            .then((quete) => {
+                console.log(quete);
+                setQuote(quete);
+                setLoadingQuote(false);
+            })
+            .catch((err) => alert(err.message));
+    }
+
 
     return (
         <div className="widgets">
-            <div className="widgets__header">
-                <h2>Latest 5 GitHub jobs</h2>
-            </div>
-            <div className="widgets__body">
-                {loadingJobs ?
-                    <CircularProgress style={{ margin: 10 }} />
-                    :
-                    jobs.length ?
-                        jobs.map((job) =>
-                            <a className="article" href={job.url} target="_blank" rel="noreferrer">
-                                <div className="article__title">{job.title}</div>
-                                <p className="article__description">
-                                    {job.location}
-                                </p>
-                            </a>
-                        )
+            <div className="widget">
+                <div className="widgets__header">
+                    <h2>Latest 5 GitHub jobs</h2>
+                </div>
+                <div className="widgets__body">
+                    {loadingJobs ?
+                        <CircularProgress style={{ margin: 10 }} />
                         :
-                        <p className="article__notAvailable">No Jobs Available</p>
-                }
+                        jobs.length ?
+                            jobs.map((job) =>
+                                <a key={job.id} className="article" href={job.url} target="_blank" rel="noreferrer">
+                                    <div className="article__title">{job.title}</div>
+                                    <p className="article__description">
+                                        {job.location}
+                                    </p>
+                                </a>
+                            )
+                            :
+                            <p className="article__notAvailable">No Jobs Available</p>
+                    }
+                </div>
+            </div>
+
+            <div className="widget">
+                <div className="widgets__header">
+                    <h2>Programming Quote</h2>
+                </div>
+                <div className="widgets__body">
+                    {loadingQuote ?
+                        <CircularProgress style={{ margin: 10 }} />
+                        :
+                        quote ?
+                            <div className="quote">
+                                <div className="quote__body">{`"${quote.quote}"`}</div>
+                                <p className="quote__author">
+                                    {quote.author}
+                                </p>
+                            </div>
+                            :
+                            <p className="quote__notAvailable">Quote not available</p>
+                    }
+                </div>
             </div>
         </div>
     )
--- src/configs/config.js
+++ src/configs/config.js
@@ -2,6 +2,7 @@
 const config = {
     urls: {
         gitHubJobs: 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json',
+        randomProgrammingQuote: 'http://quotes.stormconsultancy.co.uk/random.json',
     }
 }
 
