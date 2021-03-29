2bc7f303db8a1fc809f33a8373ff7166d0073dbe
Todor Grigorov <t.grigorov99@gmail.com>
2021-03-29 11:42:37 +0300

    Added Custom Protected Route

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const user = useSelector((state => state.user));
+
+    return (
+        user.email ?
+            <Route {...rest} render={
+                props => <Component {...rest} {...props} />
+            } />
+            :
+            <Redirect to='/' />
+
+    )
+}
+
+export default ProtectedRoute;
