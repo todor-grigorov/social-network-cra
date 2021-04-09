9c3335c6958bfa10fcf58346cceaf1bf84397a61
Todor Grigorov <t.grigorov99@gmail.com>
2021-04-10 00:08:12 +0300

    Changes to Public Home Page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,34 @@
+import { Button } from "@material-ui/core";
 import * as React from "react";
+import { Link } from "react-router-dom";
 import '../css/App.css';
-import mainImage from "../resources/images/web-2389250_1280.jpg"
+// import mainImage from "../resources/images/web-2389250_1280.jpg";
+// import mainImage from "../resources/images/thisisengineering2.jpg";
+import mainImage from "../resources/images/code.jpg";
 
 
 const HomePage = (props) => {
 
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <section className="section">
+        <div className="home__container">
+            <div className="home__text">
                 <h1 className="public-headline">Welcome to your programming community</h1>
+                <div className="home__introduction">
+                    <h2>Here you will find your fellow programmers and share your passion for programming</h2>
+                </div>
+                <div className="home__buttons">
+                    <Link to="/register">
+                        <Button variant="contained" color="primary" size="large">join now</Button>
+                    </Link>
+                    <Link to="/signin">
+                        <Button variant="contained" color="primary" size="large">sign in</Button>
+                    </Link>
+                </div>
+            </div>
+            {/* <section className="section">
                 <img className="public-main-img" src={mainImage} alt="main-pic" />
-            </section>
+            </section> */}
         </div>
     );
 }
