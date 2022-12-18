f94e09834e0975b6d2259a80c0ec66ae55b47c59
t.grigorov <t.grigorov99@gmail.com>
2022-12-18 17:00:59 +0100

    add env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,17 @@
 import firebase from 'firebase';
 import "firebase/storage";
 
+console.log(process.env);
+console.log(process.env.API_KEY);
+const { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_APP_ID, REACT_APP_FIREBASE_MESSENGING_SENDER_ID } = process.env;
 const firebaseConfig = {
-    apiKey: "AIzaSyC428VyUHH0jhuKftLrNN-mw9u_tPJAODw",
+    // apiKey: "AIzaSyC428VyUHH0jhuKftLrNN-mw9u_tPJAODw",
+    apiKey: REACT_APP_FIREBASE_API_KEY,
     authDomain: "social-network-cra.firebaseapp.com",
     projectId: "social-network-cra",
     storageBucket: "social-network-cra.appspot.com",
-    messagingSenderId: "44275772299",
-    appId: "1:44275772299:web:604a0341f18e3fa29f1d8e"
+    messagingSenderId: REACT_APP_FIREBASE_MESSENGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
