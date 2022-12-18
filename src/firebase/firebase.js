e3896d1cedcbc2a7a4b9c92ed845bc863b46b285
t.grigorov <t.grigorov99@gmail.com>
2022-12-18 17:01:26 +0100

    remove comment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,6 @@ console.log(process.env);
 console.log(process.env.API_KEY);
 const { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_APP_ID, REACT_APP_FIREBASE_MESSENGING_SENDER_ID } = process.env;
 const firebaseConfig = {
-    // apiKey: "AIzaSyC428VyUHH0jhuKftLrNN-mw9u_tPJAODw",
     apiKey: REACT_APP_FIREBASE_API_KEY,
     authDomain: "social-network-cra.firebaseapp.com",
     projectId: "social-network-cra",
